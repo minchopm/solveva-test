50d3564d31706326725f656950906a4627d2b0b9
mimsim <mimsim2000@gmail.com>
2022-03-10 17:21:12 +0200

    fe/design

--- frontend-test-excersize/frontend/src/app/catalog/particles/table/table.component.ts
+++ frontend-test-excersize/frontend/src/app/catalog/particles/table/table.component.ts
@@ -7,9 +7,108 @@ import { Content } from "../../interfaces/content";
   styleUrls: ["./table.component.scss"],
 })
 export class TableComponent implements OnInit {
-  @Input() content: Content[] = [];
+  //@Input() content: Content[] = [];
+  public contentData: Content[] = [];
+  public contentTable: Content[] = [
+    {
+      id: 1,
+      name: "Data 1",
+      value: "Value 1",
+    },
+    {
+      id: 2,
+      name: "Data 2",
+      value: "Value 2",
+    },
+    {
+      id: 3,
+      name: "Data 3",
+      value: "Value 3",
+    },
+    {
+      id: 4,
+      name: "Data 4",
+      value: "Value 4",
+    },
+    {
+      id: 1,
+      name: "Data 1",
+      value: "Value 1",
+    },
+    {
+      id: 2,
+      name: "Data 2",
+      value: "Value 2",
+    },
+    {
+      id: 3,
+      name: "Data 3",
+      value: "Value 3",
+    },
+    {
+      id: 4,
+      name: "Data 4",
+      value: "Value 4",
+    },
+    {
+      id: 1,
+      name: "Data 1",
+      value: "Value 1",
+    },
+    {
+      id: 2,
+      name: "Data 2",
+      value: "Value 2",
+    },
+    {
+      id: 3,
+      name: "Data 3",
+      value: "Value 3",
+    },
+    {
+      id: 4,
+      name: "Data 4",
+      value: "Value 4",
+    },
+    {
+      id: 1,
+      name: "Data 1",
+      value: "Value 1",
+    },
+    {
+      id: 2,
+      name: "Data 2",
+      value: "Value 2",
+    },
+    {
+      id: 3,
+      name: "Data 3",
+      value: "Value 3",
+    },
+    {
+      id: 4,
+      name: "Data 4",
+      value: "Value 4",
+    },
+  ];
 
-  constructor() {}
+  page = 1;
+  pageSize = 5;
+  collectionSize = this.contentTable.length;
 
+  constructor() {
+    this.refreshContentData();
+  }
   ngOnInit(): void {}
+  refreshContentData() {
+    this.contentData = this.contentTable
+      .map((country, i) => ({
+        // id: i + 1,
+        ...country,
+      }))
+      .slice(
+        (this.page - 1) * this.pageSize,
+        (this.page - 1) * this.pageSize + this.pageSize
+      );
+  }
 }
---
+++ frontend-test-excersize/frontend/src/assets/mock-data.ts
@@ -0,0 +1,23 @@
+import { Content } from "./../../src/app/catalog/interfaces/content";
+export const MOCKDATA: Content[] = [
+  {
+    id: 1,
+    name: "Data 1",
+    value: "Value 1",
+  },
+  {
+    id: 2,
+    name: "Data 2",
+    value: "Value 2",
+  },
+  {
+    id: 3,
+    name: "Data 3",
+    value: "Value 3",
+  },
+  {
+    id: 4,
+    name: "Data 4",
+    value: "Value 4",
+  },
+];
