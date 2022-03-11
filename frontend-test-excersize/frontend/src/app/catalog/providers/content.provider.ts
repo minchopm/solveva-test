892221492a50d3c61fe2575a30873185f1681f70
mincho.milev <mincho@elando.bg>
2022-03-11 09:46:43 +0200

    localhost update

--- frontend-test-excersize/frontend/src/app/catalog/providers/content.provider.ts
+++ frontend-test-excersize/frontend/src/app/catalog/providers/content.provider.ts
@@ -2,7 +2,6 @@ import { Injectable } from "@angular/core";
 import { HttpClient, HttpParams } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { Page } from "../interfaces/page";
-import { environment } from "src/environments/environment";
 
 @Injectable({
   providedIn: "root",
@@ -13,7 +12,7 @@ export class ContentProvider {
   getData(page: number){
     const params = new HttpParams().set("page", page);
 
-    return  this.http.get(environment.base + "/data",  {params} )as Observable<Page> ;
+    return  this.http.get("/data",  {params} )as Observable<Page> ;
   }
 
 }
